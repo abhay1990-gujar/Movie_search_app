15366faec686d796ef03461d011383b2fb45bd1d
Abhay <gujarabhay@yahoo.com>
2020-09-13 04:52:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("search");
+	
+});
+
+app.get("/results", function(req, res){
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?s=" + query + "&apikey=f3d938fa";
+	request(url, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var data =JSON.parse(body);
+			res.render("results", {data: data});
+		}
+	});
+});
+
+
+
+app.listen(3000, () => {
+	console.log("server is listening")
+});
+
+
+
+
+
+app.listen(process.env.Port, process.env.IP, function(){
+	console.log("Movie app has started");
+});
